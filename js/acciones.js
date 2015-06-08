ea2a311f61ef7c7a587991a5b09e2bfaebce0174
AL25F13 <dmc22@outlook.com>
2015-06-08 08:05:03 -0500

    cinco

--- js/acciones.js
+++ js/acciones.js
@@ -2,12 +2,14 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
+	var basedatos= window.sqliteplugin.openDataBase({name: "coloresBD.db",createFromLocation:1});
 	audio=window.plugins.LowlatencyAudio;
 		audio.preloadFX('B1','audio/C.mp3',function(){},function(msg){alert("Error"+msg);});
 		audio.preloadFX('B2','audio/D.mp3',function(){},function(msg){alert("Error"+msg);});
 		audio.preloadFX('B3','audio/E.mp3',function(){},function(msg){alert("Error"+msg);});
 		audio.preloadFX('B4','audio/F.mp3',function(){},function(msg){alert("Error"+msg);});
 	$('#btnjugar').on('tap', function(){
+		
 		var pantalla = $.mobile.getScreenHeight();
 		var encabezado = $('.ui-header').outerHeight();
 		var pie = $('.ui-footer').outerHeight();
@@ -33,11 +35,17 @@ document.addEventListener("deviceready",function(){
 		});
 		
 		
-		
-	
+		function cargarnombrejugador()
+		{
+			basedatos.transaction(function(ejecutar){
+				var sql="SELECT NombreUsuario FROM Usuario";
+				ejecutar.executeSql(sql,undefined,function(ejecutar,resultado){
+				var datosJugador=resultado.row.item(0);
+				$('#jugador').text(datosJugador.NombreUsuario);
+			
+				});
+		    });
+		}
+					
 }); 
 });
-
-
-
-
